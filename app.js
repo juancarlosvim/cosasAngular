09230a3ef27cbf95f31e36dc4ee985696ed04fe2
juancarlosvim <juancarlosvillegasmarmol@gmail.com>
2018-08-20 10:47:06 +0200

    Tipo de datos typescript

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+var nombre;
+var numero = 123;
+var booleano = true;
+var hoy = new Date();
+nombre = "Peter";
+hoy = new Date('2020-10-21');
+console.log(hoy);
+var cualquiera;
+// puede recibir nombres, booleanos, number
+var spiderman = {
+    nombre: "Peter",
+    edad: 20
+};
+// si añadimos otra propiedad al objeto no los indica mal
+spiderman = {
+    nombre: "Juan",
+    edad: 30,
+};
---
+++ app.ts
@@ -0,0 +1,22 @@
+
+let nombre:string;
+let numero:number = 123;
+let booleano:boolean = true;
+let hoy:Date = new Date();
+nombre = "Peter";
+hoy = new Date('2020-10-21');
+console.log(hoy);
+let cualquiera:any;
+// puede recibir nombres, booleanos, number
+
+let spiderman = {
+    nombre: "Peter",
+    edad: 20
+};
+// si añadimos otra propiedad al objeto no los indica mal
+
+spiderman = {
+    nombre: "Juan",
+    edad: 30,
+    //poder = "Pepito"
+};
