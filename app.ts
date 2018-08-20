2c0b44843e7e658c7dff2947fe3408182157a95a
juancarlosvim <juancarlosvillegasmarmol@gmail.com>
2018-08-20 11:17:06 +0200

    Funciones normales y funciones flecha

--- app.ts
+++ app.ts
@@ -1,12 +1,34 @@
-//funcion con 3 parametros , 1 obligatorio, 2 por defecto, y 3 opcional
-function activar(quien:string, objeto:string="función", momento?:string) {
-    let mensaje:string;
-    if(momento){
-        mensaje = `${quien} activó la ${objeto} en el ${momento}`;
-    }else{
-        mensaje = `${quien} activó la ${objeto}`;
-    }
-    console.log(mensaje);
-}
+//funciones flecha
+let miFuncion = function (a) {
+    return a;
+};
 
-activar("Peter", "", "Madrid");
+let miFuncionF = (a) =>{
+   return a;
+};
+
+let miFuncion2 = function(a:number, b:number){
+    return a+b;
+};
+
+let miFuncion2F = (a:number, b:number) =>{
+    return a+b;
+};
+
+let miFuncion3 = function(nombre:string){
+    nombre = nombre.toUpperCase();
+    return nombre;
+};
+
+let miFuncion3F = (nombre:string) =>{
+    nombre = nombre.toUpperCase();
+    return nombre;
+};
+
+
+console.log(`Mi funcion ${miFuncion("Normal")}`);
+console.log(`Mi funcion ${miFuncionF("Flecha")}`);
+console.log(`Mi funcion2 ${miFuncion2(5, 6)}`);
+console.log(`Mi funcion2F ${miFuncion2F(5, 6)}`);
+console.log(`Mi funcion3 ${miFuncion3("pedro")}`);
+console.log(`Mi funcion3F ${miFuncion3F("pedro")}`);
