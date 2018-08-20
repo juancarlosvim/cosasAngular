3c8998d17ac124a00d6c6a0445cf9c4ab95b3902
juancarlosvim <juancarlosvillegasmarmol@gmail.com>
2018-08-20 11:28:20 +0200

    Funcion flecha parte 1

--- app.ts
+++ app.ts
@@ -1,34 +1,13 @@
-//funciones flecha
-let miFuncion = function (a) {
-    return a;
+// en esta funcion nos saldrá undefined porque el this coge el de manera global si ponemos let nombre = pedro en el superior nos indicara que el nombre es pedro
+//let nombre = "pedro";
+let hulk = {
+    nombre: "Hulk",
+    smash(){
+        setTimeout(function () {
+            console.log(this.nombre + " smash!!");
+        }, 1500);
+
+    }
 };
 
-let miFuncionF = (a) =>{
-   return a;
-};
-
-let miFuncion2 = function(a:number, b:number){
-    return a+b;
-};
-
-let miFuncion2F = (a:number, b:number) =>{
-    return a+b;
-};
-
-let miFuncion3 = function(nombre:string){
-    nombre = nombre.toUpperCase();
-    return nombre;
-};
-
-let miFuncion3F = (nombre:string) =>{
-    nombre = nombre.toUpperCase();
-    return nombre;
-};
-
-
-console.log(`Mi funcion ${miFuncion("Normal")}`);
-console.log(`Mi funcion ${miFuncionF("Flecha")}`);
-console.log(`Mi funcion2 ${miFuncion2(5, 6)}`);
-console.log(`Mi funcion2F ${miFuncion2F(5, 6)}`);
-console.log(`Mi funcion3 ${miFuncion3("pedro")}`);
-console.log(`Mi funcion3F ${miFuncion3F("pedro")}`);
+hulk.smash();
