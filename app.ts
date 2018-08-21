f33223232f4ba05efeafeb50f38f847bf876ec64
juancarlosvim <juancarlosvillegasmarmol@gmail.com>
2018-08-21 09:39:15 +0200

    repaso de Typescript

--- app.ts
+++ app.ts
@@ -1,17 +1,58 @@
-class Avenger {
-    nombre:string;
-    equipo:string;
-    nombreReal:string;
-    puedePelear:boolean = false;
-    peleasGanadas:number = 0;
+// Uso de Let y Const
+let nombre = "Ricardo Tapia";
+let edad = 23;
 
-    constructor(nombre:string, equipo:string, nombreReal:string){
-       this.nombre = nombre;
-       this.equipo = equipo;
-       this.nombreReal = nombreReal;
-    }
+const PERSONAJE = {
+    nombre: nombre,
+    edad: edad
+};
+
+
+// Cree una interfaz que sirva para validar el siguiente objeto
+let batman = {
+    nombre: "Bruno Díaz",
+    artesMarciales: ["Karate","Aikido","Wing Chun","Jiu-Jitsu"]
+};
+
+interface Batman {
+    nombre:string,
+    artesMarciales:string[]
 }
 
-let antMan:Avenger = new Avenger('Antman','cap','Scott Lang');
+// Convertir esta funcion a una funcion de flecha
+let resultadoDoble = ( a:number, b:number ) => {
+    return (a + b) * 2
+};
 
-console.log(antMan);
+
+// Función con parametros obligatorios, opcionales y por defecto
+// donde NOMBRE = obligatorio
+//       PODER  = opcional
+//       ARMA   = por defecto = "arco"
+function getAvenger( nombre:string, poder?:string, arma:string="arco" ){
+    let mensaje:string;
+    if( poder ){
+        mensaje = `${nombre} tiene el poder de: ${poder} y un arma ${arma}`;
+    }else{
+        mensaje = `${nombre} tiene un ${poder};`
+    }
+};
+
+// Cree una clase que permita manejar la siguiente estructura
+// La clase se debe de llamar rectangulo,
+// debe de tener dos propiedades:
+//   * base
+//   * altura
+// También un método que calcule el área  =  base * altura,
+// ese método debe de retornar un numero.
+
+class Rectangulo {
+    base:number=0;
+    altura:number=0;
+
+    calcularArea():number{
+        return this.base * this.altura;
+    }
+
+
+}
